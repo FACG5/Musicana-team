dfec023c5e71f5f21090f1028727e8376216f790
Afal <afal@live.com>
2018-07-18 13:28:10 +0300

    logic integrated by afal

--- public/js/DOM.js
+++ public/js/DOM.js
@@ -4,6 +4,9 @@
     var view = document.getElementById("result");
     var image = document.getElementById("image");
     var number = document.getElementById("number");
+    var audioTool = document.querySelector('.audiotool');
+    
+    var lyrics = document.querySelector('.lyrics');
     var mykey = config.MY_KEY;
     search.addEventListener('click', function() {  
         var limit = number.value;
@@ -19,13 +22,44 @@
                 var name = obj.results.trackmatches.track[i].name;
                 var li = document.createElement("LI");
                 var newLink = document.createElement("a");
-                newLink.setAttribute('href', '#');
+
+                
                 newLink.innerHTML = name;
                 view.appendChild(li);
                 li.appendChild(newLink);
 
             }
+
+            
+
+
         })
     })
+
+    view.addEventListener('click',function(e){
+
+        var keywords = e.target.textContent.split(' ').join('+');
+        var mp4link;
+        var alyrics;
+
+        fetch('https://itunes.apple.com/search?term='+keywords+'&limit=1', function(obj2){
+            mp4link=obj2.results[0].previewUrl;
+            console.log('music link: ' + mp4link);
+            audioTool.src=mp4link;
+            audioTool.play();
+            var artistName = obj2.results[0].artistName.split(' ').join('+');
+            var trackName = obj2.results[0].trackName.split(' ').join('+');
+            fetch('https://api.lyrics.ovh/v1/'+ artistName + '/'+ trackName,function(obj3){
+        
+                lyrics.innerHTML = obj3.lyrics;  
+            });
+        });
+
+
+
+        
+    });
+
+
     
 })();
