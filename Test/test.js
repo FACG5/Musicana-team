3bec28276210ca1758fcc54e8b80b0c0b66a2927
Mohannad <mohannad.hanafi.b@hotmail.com>
2018-07-18 11:40:22 +0300

    Made one test

--- Test/test.js
+++ Test/test.js
@@ -2,6 +2,11 @@ var test = require('tape');
 var logic = require('../public/js/logic');
 
 test('testing', function(s){
-    s.equal(typeof 'hello', 'string', 'one should equal itself');
+    var url = "http://ws.audioscrobbler.com/2.0/?method=track.search&track=%20&artist=Adele&format=json&api_key=a4e7f14385fcf0448c9693394bd04c0e&limit=5";
+    logic(url, function(res){
+        var actual = res.results.trackmatches.track[0].name;
+        var expected = "Rolling in the Deep";
+        s.equal(actual, expected, 'one should equal itself');
     s.end();
+    });
 });
